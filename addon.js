b2e11250ffe2da41973d75c2e6974460fcdfc820
Ritz, Ryan W <Ryan.Ritz@pearson.com>
2014-07-31 14:38:03 -0600

    Addon update

--- addon.js
+++ addon.js
@@ -2,7 +2,7 @@ var addon = require('./build/Release/led-wrapper');
 //var pixeladdon = require('.build/Release/pixelobject');
 
 var obj = new addon.LedWrapper();
-var pixel = new addon.PixelObject(0,0,0,0,0);
+var pixel = new addon.PixelObject(16,16,250,250,250);
 console.log(typeof obj, pixel, obj.proto);
 obj.setPixels(pixel);
 //obj.SetPixels(pixel);
